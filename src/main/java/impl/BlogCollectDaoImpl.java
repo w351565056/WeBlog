53d58c58748eae6f5e3fd7f0d3cfd5ba9f964761
Wiggin <351565056@qq.com>
2018-08-29 22:45:08 +0800

    规范Dao，entity

---
+++ src/main/java/impl/BlogCollectDaoImpl.java
@@ -0,0 +1,9 @@
+package impl;
+
+import dao.BlogCollectDao;
+import entity.BlogCollect;
+import util.BaseDao;
+
+public class BlogCollectDaoImpl extends BaseDao<BlogCollect> implements BlogCollectDao {
+
+}
--- src/main/java/impl/BlogContentDaoImpl.java
+++ src/main/java/impl/BlogContentDaoImpl.java
@@ -4,10 +4,26 @@ import dao.BlogContentDao;
 import entity.BlogContent;
 import util.BaseDao;
 
+import java.util.List;
+
 public class BlogContentDaoImpl extends BaseDao<BlogContent> implements BlogContentDao {
     @Override
     public void InsertBlog(BlogContent blog) {
         executeUpdate("insert into blog_content(user_id,blog_text,blog_img) values(?,?,?)",
                 new Object[]{blog.getUSER_ID(),blog.getBLOG_TEXT(),blog.getBLOG_IMG()});
+    }
+    @Override
+    public List<BlogContent> ShowContent(int i) {
+        return executeQuery("select * from BLOG_CONTENT where BLOG_ID < ?",new Object[]{(i+1)});
+    }
+
+    @Override
+    public List<BlogContent> ShowContent(String str, int i) {
+        return null;
+    }
+
+    @Override
+    public List<BlogContent> ShowContent() {
+        return null;
     }
 }
