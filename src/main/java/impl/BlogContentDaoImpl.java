1a84d306b06eed03ab3ffb459f7f698b9eaf2a89
ht000 <1029279310@qq.com>
2018-08-30 09:44:24 +0800

    Add BlogContent;

--- src/main/java/impl/BlogContentDaoImpl.java
+++ src/main/java/impl/BlogContentDaoImpl.java
@@ -15,7 +15,7 @@ public class BlogContentDaoImpl extends BaseDao<BlogContent> implements BlogCont
     }
     @Override
     public List<BlogContent> ShowContent() {
-        return executeQuery("select * from BLOG_CONTENT");
+        return executeQuery("select USER_INFO.USER_NAME,USER_INFO.HEAD_IMG,BLOG_CONTENT.* from USER_INFO,BLOG_CONTENT where BLOG_CONTENT.USER_ID = USER_INFO.USER_ID");
     }
 
     @Override
