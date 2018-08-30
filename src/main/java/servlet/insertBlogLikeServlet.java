0101f68161fe5fb60e028be8b288adae47cf4a7b
ht000 <1029279310@qq.com>
2018-08-30 16:45:24 +0800

    Add BlogContent;

--- src/main/java/servlet/insertBlogLikeServlet.java
+++ src/main/java/servlet/insertBlogLikeServlet.java
@@ -1,5 +1,9 @@
 package servlet;
 
+import dao.BlogLikeDao;
+import entity.BlogLike;
+import impl.BlogLikeDaoImpl;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,9 +11,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "insertBlogLikeServlet")
+@WebServlet("/insertBlogLikeServlet")
 public class insertBlogLikeServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        BlogLikeDao insertBlogLike = new BlogLikeDaoImpl();
+
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
