d948cf3f7c98fc8d4359a716f01a8517b9cdb2c2
ht000 <1029279310@qq.com>
2018-09-02 15:52:44 +0800

    Add BlogContent;

--- src/main/java/servlet/showCollectServlet.java
+++ src/main/java/servlet/showCollectServlet.java
@@ -22,18 +22,18 @@ import java.util.List;
 public class ShowCollectServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         BigDecimal blogid = new BigDecimal(request.getParameter("blog_id"));
+        BlogCollectQuery blogCollectQuery =new BlogCollectQuery();
+        blogCollectQuery.setBLOG_ID(blogid);
         BlogCollectDao showBlogCollect = new BlogCollectDaoImpl();
-        BlogCollectQuery blogCollect = new BlogCollectQuery();
-        blogCollect.setBLOG_ID(blogid);
-        List<BlogCollectQuery> list = showBlogCollect.ShowCollectBlog(blogCollect);
+        List<BlogCollectQuery>  list= showBlogCollect.ShowCollectBlog(blogCollectQuery);
         JsonConfig jsonConfig = new JsonConfig();
         jsonConfig.registerJsonValueProcessor(Date.class,new JsonDateValueProcessor());
         JSONArray array = JSONArray.fromObject(list,jsonConfig);
-        response.getWriter().print(array);
-//        PrintWriter out = response.getWriter();
-//        out.print(array);
-//        out.flush();
-//        out.close();
+//        response.getWriter().print(array);
+        PrintWriter out = response.getWriter();
+        out.print(array);
+        out.flush();
+        out.close();
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
